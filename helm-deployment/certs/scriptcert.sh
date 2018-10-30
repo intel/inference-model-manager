c19115517a474d7fc4a2db2937ea15eb95796a83
Karolina Sankiewicz <karolina.sankiewicz@intel.com>
2018-10-30 15:29:19 +0100

    Kops flow  (#140)

--- helm-deployment/certs/scriptcert.sh
+++ helm-deployment/certs/scriptcert.sh
@@ -3,15 +3,26 @@
 # This script shows how to generate test keys and certs
 # If you provide different value in annotations.allowed values than "CN=client", remember to change subject name also in client key
 
-# Generate server key/cert
-openssl genrsa -out server.key 4096
-openssl req -new -x509 -days 365 -key server.key -out server.crt -subj "/CN=serving-service.kube.cluster"
-
 # Generate valid CA
-openssl genrsa -out ca-cert-secret.key 4096
-openssl req -new -x509 -days 365 -key ca-cert-secret.key -out ca-cert-secret.crt -subj "/CN=ca"
+openssl genrsa -out ca.key 4096
+openssl req -new -x509 -days 365 -key ca.key -out ca.crt -subj "/CN=ca"
 
 # Generate valid client key/cert
 openssl genrsa -out client.key 4096
 openssl req -new -key client.key -out client.csr -subj "/CN=client"
-openssl x509 -req -days 365 -in client.csr -CA ca-cert-secret.crt -CAkey ca-cert-secret.key -set_serial 01 -out client.crt
+openssl x509 -req -days 365 -in client.csr -CA ca.crt -CAkey ca.key -set_serial 01 -out client.crt
+
+# Generate valid server key/cert
+openssl genrsa -out server.key 4096
+openssl req -new -key sesrver.key -out server.csr -subj "/CN=serving-service.kube.cluster"
+openssl x509 -req -days 365 -in server.csr -CA ca.crt -CAkey ca.key -set_serial 01 -out server.crt
+
+# Generate valid CA for dex
+openssl genrsa -out ca-cert-secret-dex.key 4096
+openssl req -new -x509 -days 365 -key ca-cert-secret-dex.key -out ca-cert-secret-dex.crt -subj "/CN=ca-dex"
+
+# Generate valid dex server key/cert
+openssl genrsa -out dex.key 4096
+openssl req -new -key dex.key -out dex.csr -subj "/CN=*.${DOMAIN_NAME}"
+openssl x509 -req -days 365 -in dex.csr -CA ca-cert-secret-dex.crt -CAkey ca-cert-secret-dex.key -set_serial 01 -out server-dex.crt
+
---
+++ kops/SETUP.md
@@ -0,0 +1,40 @@
+
+## CREATE CLUSTER MANUALLY
+```
+wget -O kops https://github.com/kubernetes/kops/releases/download/$(curl -s https://api.github.com/repos/kubernetes/kops/releases/latest | grep tag_name | cut -d '"' -f 4)/kops-linux-amd64
+
+chmod +x ./kops
+sudo mv ./kops /usr/local/bin/
+export PROJECT=`gcloud config get-value project`
+export KOPS_FEATURE_FLAGS=AlphaAllowGCE
+export GOOGLE_APPLICATION_CREDENTIALS="/home/local/GER/username/.config/gcloud/legacy_credentials/user.name@intel.com/adc.json"
+
+export CLUSTER_NAME="<clustername>"
+
+sed -i "s/toreplacebyclustername/${CLUSTER_NAME}/g" desired.yaml
+kops create -f desired.yaml
+export KOPS_STATE_STORE=gs://kubernetes-clusters-inferno
+kops update cluster <clustername>.k8s.local --yes
+export NAME=<cluster-name>.k8s.local
+kops export kubecfg ${NAME}
+
+kubectl create sa tiller --namespace kube-system
+kubectl create clusterrolebinding tiller-cluster-rule --clusterrole=cluster-admin --serviceaccount=kube-system:tiller
+helm init --debug --upgrade --service-account tiller
+
+cd ../helm-deployment/
+helm dep up .
+helm install --name inferno-platform .
+
+while [ -z $ING_IP ]; do sleep 10; ING_IP=$(kubectl get ing -o=jsonpath='{.items[0].status.loadBalancer.ingress..ip}'); done
+
+echo $ING_IP
+```
+## ADD A RECORD IN AWS ROUTE 53
+
+```
+aws route53 change-resource-record-sets --hosted-zone-id Z11DOV0M5AJEBB --change-batch file://route_record.json
+```
+where route_record.json is a json file containing ingress ip (echo $ING_IP) and your domain name (example in /inferno-platform/kops directory)
+
+
