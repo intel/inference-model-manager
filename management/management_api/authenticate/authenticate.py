ea05d10dbe440f235411af1974622d3c44e335d9
Trawinski, Dariusz <dariusz.trawinski@intel.com>
2018-11-22 11:07:44 +0100

    license file and headers (#209)
    * added license file and headers
    * fix style check

--- management/management_api/authenticate/authenticate.py
+++ management/management_api/authenticate/authenticate.py
@@ -1,3 +1,19 @@
+#
+# Copyright (c) 2018 Intel Corporation
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
 import falcon
 import json
 import time
@@ -54,6 +70,7 @@ class TokenDecoder:
                 return self.jwt.decode(token, self.keys[self.key_number])
             except Exception as e:
                 logger.info("Failed to decode token with default key")
+                logger.debug(e)
                 pass
         for idx, key in enumerate(self.keys):
             try:
