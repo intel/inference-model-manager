10d148bd5422a2c5ac53cf0abface735854bca40
Karolina Sankiewicz <karolina.sankiewicz@intel.com>
2019-01-23 09:25:03 +0100

    Add verification of platform before running tests (#277)

--- scripts/test_imm.sh
+++ scripts/test_imm.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
 TENANT_NAME="tenanttest"
 ENDPOINT_NAME="endpointtest"
@@ -9,7 +9,6 @@ MODEL_VERSION_POLICY="{latest{}}"
 ADMIN_SCOPE="test"
 USER_SCOPE="test"
 REPLICAS=2
-IMM_CONFIG_PATH=~/.imm
 MODEL_PATH="saved_model.pb"
 NUMPY_PATH="10_v1_imgs.npy"
 TESTS_NUMBER=0
@@ -19,32 +18,13 @@ SERVER_CERT="../helm-deployment/management-api-subchart/certs/server-tf.crt"
 CLIENT_CERT="../helm-deployment/management-api-subchart/certs/client-tf.crt"
 CLIENT_KEY="../helm-deployment/management-api-subchart/certs/client-tf.key"
 
-get_token() {
-	user=$1
-	echo "Get $user token"
-	TOKEN=`python -W ignore ../tests/management_api_tests/authenticate.py $user`
-	TOKEN=`echo $TOKEN | tr \' \"`
-	ACCESS_TOKEN=`echo $TOKEN | jq -r '.access_token'`
-	TOKEN_TYPE=`echo $TOKEN | jq -r '.token_type'`
-	EXPIRES_IN=`echo $TOKEN | jq -r '.expires_in'`
-	REFRESH_TOKEN=`echo $TOKEN | jq -r '.refresh_token'`
-	ID_TOKEN=`echo $TOKEN | jq -r '.id_token'`
-	EXPIRES_AT=`echo $TOKEN | jq -r '.expires_at'`
-	IMM_CFG="{\"management_api_port\": 443, \"management_api_address\": \"${MGMT_DOMAIN_NAME}\", \"ca_cert_path\": \"null\", \"access_token\": \"${ACCESS_TOKEN}\", \"id_token\": \"${ID_TOKEN}\", \"token_type\": \"${TOKEN_TYPE}\", \"refresh_token\": \"${REFRESH_TOKEN}\", \"expires_in\": \"${EXPIRES_IN}\", \"expires_at\": \"${EXPIRES_AT}\"}"
-	echo ${IMM_CFG} > ${IMM_CONFIG_PATH}
-	echo "Token is saved in ${IMM_CONFIG_PATH}"
-}
+SAVED_MODEL_SRC="https://storage.googleapis.com/inference-eu/models_zoo/resnet_V1_50/saved_model/saved_model.pb"
+IMAGES_NUMPY_SRC="https://storage.googleapis.com/inference-eu/models_zoo/resnet_V1_50/datasets/10_v1_imgs.npy"
 
-remove_resources() {
-    error_message=$1
-    ./imm rm t ${TENANT_NAME}
-    ./imm logout
-    echo "Tests failed. Error occurred during ${error_message} test" 
-    exit 1
-}
+[[ ! -f ${MODEL_PATH} ]] && echo "Downloading model" && wget ${SAVED_MODEL_SRC}
+[[ ! -f ${NUMPY_PATH} ]] && echo "Downloading numpy images" && wget ${IMAGES_NUMPY_SRC}
 
-[[ ! -f ${MODEL_PATH} ]] && echo "Downloading model" && wget https://storage.googleapis.com/inference-eu/models_zoo/resnet_V1_50/saved_model/saved_model.pb 
-[[ ! -f ${NUMPY_PATH} ]] && echo "Downloading numpy images" && wget https://storage.googleapis.com/inference-eu/models_zoo/resnet_V1_50/datasets/10_v1_imgs.npy
+. ./imm_utils.sh
 
 echo "****************************ADMIN****************************"
 get_token admin
@@ -70,7 +50,7 @@ get_token user
 echo "Model upload"
 response=`./imm u ${MODEL_PATH} ${MODEL_NAME} ${MODEL_VERSION} ${TENANT_NAME}`
 let "TESTS_NUMBER++"
-[[ $response =~ "completed successfully" ]] && { echo "Test passed" && let "PASSED_TESTS++"; } || { echo "Test failed: $response" && remove_resources "model upload"; }
+[[ $response =~ "completed successfully" ]] && { echo "Test passed" && let "PASSED_TESTS++"; } || { echo "Test failed: $response" && remove_resources "model upload" ${TENANT_NAME}; }
 
 echo "List models"
 response=`./imm ls m ${TENANT_NAME}`
@@ -80,7 +60,7 @@ let "TESTS_NUMBER++"
 echo "Create endpoint"
 response=`./imm c e ${ENDPOINT_NAME} ${MODEL_NAME} ${MODEL_VERSION_POLICY} ${TENANT_NAME} ${SERVING_NAME}`
 let "TESTS_NUMBER++"
-[[ $response =~ "${ENDPOINT_NAME}-${TENANT_NAME}.${DOMAIN_NAME}" ]] && { echo "Test passed" && let "PASSED_TESTS++"; } || { echo "Test failed: $response" && remove_resources "create endpoint"; }
+[[ $response =~ "${ENDPOINT_NAME}-${TENANT_NAME}.${DOMAIN_NAME}" ]] && { echo "Test passed" && let "PASSED_TESTS++"; } || { echo "Test failed: $response" && remove_resources "create endpoint" ${TENANT_NAME}; }
 
 echo "List endpoints"
 response=`./imm ls e ${TENANT_NAME}`
