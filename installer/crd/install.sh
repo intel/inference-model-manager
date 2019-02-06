7da4c20ad5558698aa356c5b138db1b99c90c674
Krzysztof Balka <krzysztof.balka@intel.com>
2019-02-06 14:26:52 +0100

    Improve crd installer

--- installer/crd/install.sh
+++ installer/crd/install.sh
@@ -1,23 +1,9 @@
 . ../utils/fill_template.sh
+. ../utils/messages.sh
 
-cd ../../server-controller
-
-REGISTRY_URL=$1
-export IMAGE=$2
-export TAG=$3
-DOMAIN_NAME=$4
-make docker_build
-NAME_WITH_REG=${REGISTRY_URL}${IMAGE}
-WHOLE_TAG=${NAME_WITH_REG}:${TAG}
-docker tag $IMAGE:$TAG ${REGISTRY_URL}${IMAGE}:${TAG}
-docker push $WHOLE_TAG
-cd -
-
+header "Installation of CRD Controller"
 cd ../../helm-deployment/crd-subchart
-
-fill_template "<crd_image_path>" $NAME_WITH_REG values.yaml
-fill_template "<crd_image_tag>" $TAG values.yaml
 fill_template "<dns_domain_name>" $DOMAIN_NAME values.yaml
 helm install .
+show_results $? "CRD installation completed" "Failed to install CRD"
 cd -
-pwd
