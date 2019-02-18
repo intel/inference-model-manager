acd8ab10ed5a23b1e6fa94bc7eb717d1f10da382
beataszelag <beata.szelag@intel.com>
2019-02-18 09:22:20 +0100

    Updated readme about exports and certificates

--- installer/k8s/create_cluster.sh
+++ installer/k8s/create_cluster.sh
@@ -15,7 +15,6 @@ header "Installing kubernetes cluster"
 CLUSTER_NAME=$1
 GCE_REGION=$2
 cp cluster_template.yaml cluster.yaml
-sed -i "s/us-west1/northamerica-northeast1/g" cluster_template.yaml
 fill_template toreplacebyclustername $CLUSTER_NAME cluster.yaml
 fill_template us-west1 ${GCE_REGION} cluster.yaml
 kops create -f cluster.yaml
