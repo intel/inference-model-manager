4dbbf172a1ad14f1ccce32c693b2b3efcead19bb
joannano <joanna.nosek@intel.com>
2019-03-01 14:27:03 +0100

    After review

--- server-controller/hooks_test.go
+++ server-controller/hooks_test.go
@@ -4,69 +4,14 @@ import (
 	"bytes"
 	"errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	"k8s.io/apimachinery/pkg/runtime"
+
 	"log"
 	"testing"
-
-	"github.com/intel/crd-reconciler-for-kubernetes/pkg/resource"
-	"github.com/intel/crd-reconciler-for-kubernetes/pkg/states"
 
 	crv1 "github.com/IntelAI/inference-model-manager/server-controller/apis/cr/v1"
 	"os"
 	"strings"
 )
-
-type mockClient struct {
-	err error
-}
-
-func NewMockClient(err error) resource.Client {
-	return &mockClient{err}
-}
-
-func (*mockClient) Reify(templateValues interface{}) ([]byte, error) {
-	return nil, nil
-}
-
-func (c *mockClient) Create(namespace string, templateValues interface{}) error {
-	return c.err
-}
-
-func (*mockClient) Delete(namespace string, name string) error {
-	return nil
-}
-
-func (*mockClient) Update(namespace string, name string, templateValues interface{}) error {
-	return nil
-}
-
-func (*mockClient) Patch(namespace string, name string, data []byte) error {
-	return nil
-}
-
-func (*mockClient) Get(namespace, name string) (runtime.Object, error) {
-	return nil, nil
-}
-
-func (*mockClient) List(namespace string, labels map[string]string) ([]metav1.Object, error) {
-	return nil, nil
-}
-
-func (*mockClient) IsFailed(namespace string, name string) bool {
-	return true
-}
-
-func (*mockClient) IsEphemeral() bool {
-	return true
-}
-
-func (*mockClient) Plural() string {
-	return ""
-}
-
-func (*mockClient) GetStatusState(runtime.Object) states.State {
-	return states.Pending
-}
 
 type clientErr struct {
 	configMapError        error
@@ -75,7 +20,7 @@ type clientErr struct {
 	ingressClientError    error
 }
 
-var inferenceEndpointsTest = []struct {
+var inferenceEndpointsTestAdd = []struct {
 	name              string
 	inferenceEndpoint crv1.InferenceEndpoint
 	expected          string
@@ -159,7 +104,7 @@ func serverAdd(infer crv1.InferenceEndpoint, errs clientErr) string {
 }
 
 func TestAdd(t *testing.T) {
-	for _, tt := range inferenceEndpointsTest {
+	for _, tt := range inferenceEndpointsTestAdd {
 		t.Run(tt.name, func(t *testing.T) {
 			got := serverAdd(tt.inferenceEndpoint, tt.clientErrors)
 			if !strings.Contains(got, tt.expected) {
---
+++ server-controller/mock_client.go
@@ -0,0 +1,61 @@
+package main
+
+import (
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+
+	"github.com/intel/crd-reconciler-for-kubernetes/pkg/resource"
+	"github.com/intel/crd-reconciler-for-kubernetes/pkg/states"
+	"k8s.io/apimachinery/pkg/runtime"
+)
+
+type mockClient struct {
+	err error
+}
+
+func NewMockClient(err error) resource.Client {
+	return &mockClient{err}
+}
+
+func (*mockClient) Reify(templateValues interface{}) ([]byte, error) {
+	return nil, nil
+}
+
+func (c *mockClient) Create(namespace string, templateValues interface{}) error {
+	return c.err
+}
+
+func (*mockClient) Delete(namespace string, name string) error {
+	return nil
+}
+
+func (*mockClient) Update(namespace string, name string, templateValues interface{}) error {
+	return nil
+}
+
+func (*mockClient) Patch(namespace string, name string, data []byte) error {
+	return nil
+}
+
+func (*mockClient) Get(namespace, name string) (runtime.Object, error) {
+	return nil, nil
+}
+
+func (*mockClient) List(namespace string, labels map[string]string) ([]metav1.Object, error) {
+	return nil, nil
+}
+
+func (*mockClient) IsFailed(namespace string, name string) bool {
+	return true
+}
+
+func (*mockClient) IsEphemeral() bool {
+	return true
+}
+
+func (*mockClient) Plural() string {
+	return ""
+}
+
+func (*mockClient) GetStatusState(runtime.Object) states.State {
+	return states.Pending
+}
