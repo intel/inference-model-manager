81fca1167d7f18b26c0cf024b22239d3a167f999
ksankiew <karolina.sankiewicz@intel.com>
2019-03-15 14:37:09 +0100

    update requirements installation

--- scripts/prepare_test_env.sh
+++ scripts/prepare_test_env.sh
@@ -20,6 +20,7 @@ export DEX_DOMAIN_NAME="dex.$DOMAIN_NAME"
 export MGMT_DOMAIN_NAME="mgt.$DOMAIN_NAME"
 
 sudo pip install -r ./requirements.txt
+sudo pip install -r ../tests/requirements.txt
 
 echo "Fetching CA for $MGMT_DOMAIN_NAME"
 #./get_cert.sh $MGMT_DOMAIN_NAME ca-ing $PROXY > ca.pem
